529a76bc8ad7fc0a8bc4e37df6bdbb8f71826985
Jalen Francis <jalenfrancis@protonmail.com>
2025-12-23 13:29:40 -0500

    Remove census import from loaders __init__

--- src/loaders/__init__.py
+++ src/loaders/__init__.py
@@ -1,6 +1,5 @@
 from .gtfs import GTFSLoader
 from .realtime import GTFSRealtimeLoader, COTA_FEEDS
-from .census import CensusLoader
 
-__all__ = ['GTFSLoader', 'GTFSRealtimeLoader', 'COTA_FEEDS', 'CensusLoader']
+__all__ = ['GTFSLoader', 'GTFSRealtimeLoader', 'COTA_FEEDS']
 
